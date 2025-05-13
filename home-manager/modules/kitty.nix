35814e3876204fe1dcf09c0994f533822c664110
kizzy <kizzytastic@gmail.com>
2025-05-13 22:51:05 +0200

    Kitty tab bar style

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -1,15 +1,19 @@
 # kitty.nix
 # Home manager kit config settings
-
-{ lib, ... }:
-
-{
+{ lib, ... }: {
   programs.kitty = lib.mkForce {
     enable = true;
     settings = {
       dynamic_background_opacity = true;
       background_opacity = "0.9";
-      background_blur = 1;
+      background_blur = 5;
+
+      tab_bar_min_tabs = 1;
+      tab_bar_edge = "bottom";
+      tab_bar_style = "powerline";
+      tab_powerline_style = "slanted";
+      tab_title_template =
+        "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
     };
   };
 }
